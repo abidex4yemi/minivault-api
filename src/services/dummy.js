89b3a63f56a98037653de209ddaa9c6e41220225
abidex4yemi <abidex4yemi@gmail.com>
2025-07-10 16:40:53 +0100

    Feat: create dummy response service

---
+++ src/services/dummy.js
@@ -0,0 +1,28 @@
+const streamDummyResponse = async (prompt, response, logFn) => {
+  const tokens = [
+    'Hello',
+    'there',
+    '!',
+    ' ',
+    'This',
+    'is',
+    'a',
+    'test',
+    'response',
+    '.',
+  ];
+
+  let completeResponse = '';
+
+  for (let token of tokens) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    response.write(`data: ${JSON.stringify(token)}\n\n`);
+    completeResponse += token;
+  }
+
+  response.write(`data: [DONE]\n\n`);
+  response.end();
+  logFn(completeResponse);
+};
+
+module.exports = { streamDummyResponse };
